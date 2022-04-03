aed7c10e3b82cc039840789cad23cdab20285507
juanmadd99 <80063560+juanmadd99@users.noreply.github.com>
2022-04-03 23:17:08 +0200

    Add files via upload

--- Visualizacion_resultados/ver_resultados_profesores.php
+++ Visualizacion_resultados/ver_resultados_profesores.php
@@ -7,48 +7,51 @@
 	$conexion=mysqli_connect("127.0.0.1","root","","bdp1");
 	if($conexion){
 			//Consulta para conseguir los temas que tiene una asignatura
-			$consulta0="SELECT ID_examen FROM examen WHERE ID_tema=$id_tema";
-			$consulta2="SELECT nombre from asignatura WHERE ID_asignatura=$id_Asignatura";
-			$consulta3="SELECT nombre from tema WHERE ID_tema=$id_tema";
+			$consulta1="SELECT nombre from asignatura WHERE ID_asignatura=$id_Asignatura";
+			$consulta2="SELECT nombre from tema WHERE ID_tema=$id_tema";
 
-			$resultado0=mysqli_query($conexion,$consulta0);
+			$resultado1=mysqli_query($conexion,$consulta1);
 			$resultado2=mysqli_query($conexion,$consulta2);
-			$resultado3=mysqli_query($conexion,$consulta3);
 	}
 	else{
-		echo "<h2>Error de conexion con la Base de datos</h2>";
+		echo "<h2>Error de conexi&oacute;n con la Base de datos</h2>";
 	}
-	$nombre_Asignatura=mysqli_fetch_row($resultado2);
-	$nombre_tema=mysqli_fetch_row($resultado3);
-	$id_examen=mysqli_fetch_row($resultado0);
-
+	$nombre_Asignatura=mysqli_fetch_row($resultado1);
+	$nombre_tema=mysqli_fetch_row($resultado2);
+	$examen_realizado=true;
+	
 	if($conexion){
-		$consulta1="SELECT Nota from estudiante_examen WHERE ID_examen=$id_examen[0]";
-		$resultado=mysqli_query($conexion,$consulta1);
+		$consulta="SELECT estudiante_examen.Nota from estudiante_examen, examen WHERE estudiante_examen.ID_examen=examen.ID_examen AND examen.ID_tema=$id_tema";
+		$resultado=mysqli_query($conexion,$consulta);
+
+		if(mysqli_num_rows($resultado)==0){
+			$examen_realizado=false;
+		}
 	}
-	echo "<h2>Media global del examen del tema: "; 
+	echo "<h1>$nombre_Asignatura[0]</h1>";
+	echo "<br>";
+	echo "<h2>Resultados globales del examen del tema "; 
 	echo $nombre_tema[0];
 	echo "</h2>";
 
-	$cont=0;
-	$suma=0;
-	while($row=mysqli_fetch_row($resultado)){
-		$suma=$suma+$row[0];
-		$cont++;
-	}
+	if($examen_realizado){
+		$cont=0;
+		$suma=0;
 
-	echo "Nota media del tema: ";
-	echo $suma/$cont;
-	echo '<br>';
+		while($row=mysqli_fetch_row($resultado)){
+			$suma=$suma+$row[0];
+			$cont++;
+		}
+
+		echo "Nota media del tema: ";
+		echo $suma/$cont;
+	}
+	else{
+		echo "El examen del tema ".$nombre_tema[0]." de la asignatura ".$nombre_Asignatura[0]." no se ha realizado a&uacute;n";
+	}
 	echo '<br>';
 	echo "<a href = '../Login/indexPro.php?id=$id_Asignatura'><input type = 'button' value = 'Volver'></a>";
 	
 mysqli_close($conexion);
 
 ?>
-
-<!DOCTYPE html>
-<head>
-	<title>Resultados</title>
-</head>
-</html>
